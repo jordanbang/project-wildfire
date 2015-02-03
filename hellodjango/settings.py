2b6b7a598a8af410d2e2c77c9c8e334cbc33a2c2
Jordan B <jordan.bangia@gmail.com>
2015-02-03 00:09:32 -0500

    Updated settings + requirements to include Django Rest Framework

--- hellodjango/settings.py
+++ hellodjango/settings.py
@@ -36,6 +36,8 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'wildfire',
+    'rest_framework',
 )
 
 MIDDLEWARE_CLASSES = (
@@ -93,7 +95,6 @@ SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 ALLOWED_HOSTS = ['*']
 
 # Static asset configuration
-import os
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 STATIC_ROOT = 'staticfiles'
 STATIC_URL = '/static/'
