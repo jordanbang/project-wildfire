7e8eb3023fb32b34b53ab3fff18b2080d06a0b9e
Jordan Bangia <george.doe231@gmail.com>
2015-02-05 18:57:05 -0500

    Updated admin site to be able to view database

--- wildfire/admin.py
+++ wildfire/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
+from wildfire.models import User, Question, Categories, MultipleChoiceOptions, RangeOptions, Answers
 # Register your models here.
+admin.site.register(User)
+admin.site.register(Question)
+admin.site.register(Categories)
+admin.site.register(MultipleChoiceOptions)
+admin.site.register(RangeOptions)
+admin.site.register(Answers)
