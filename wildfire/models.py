31ca3c2be1c18990f324daa780fe7aadbc22a32e
JoelTham <joel.tham@live.com>
2015-02-04 23:58:54 -0500

    Updated models.py
    Added basic User and Question models (still more attributes to be added
    to both)

--- wildfire/models.py
+++ wildfire/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+class User(models.model)
+	id = models.IntegerField()
+	username = models.CharField(max_length = 20)
+	password = models.CharField(max_length = 20)
+	age = models.IntegerField()
+	gender = models.BooleanField()
+	join_date = models.DateTimeField()
+	
+
+class Question(models.model)
+	user = models.ForeignKey(User)
+	id = models.IntegerField()
+	text = models.CharField(max_length = 200)
+	type = models.CharField(max_length = 20)
+	date = models.DateTimeField()
