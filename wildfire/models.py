96cb84ed0a5bf729d97af75459a23428edb364c1
Jordan B <jordan.bangia@gmail.com>
2015-02-07 14:02:30 -0500

    fixed settings for heroku

--- wildfire/models.py
+++ wildfire/models.py
@@ -16,18 +16,6 @@ class User(models.Model):
 		self.password = password
 		return self
 
-class Question(models.Model):
-	asker = models.ForeignKey(User)
-	text = models.CharField(max_length = 200)
-	question_type = models.CharField(max_length = 20)
-	date = models.DateTimeField(auto_now=True)
-	multiple_choice_options = models.ForeignKey(MultipleChoiceOption)
-	range_options = models.ForeignKey(RangeOption)
-	
-class Category(models.Model):
-	question = models.ManyToManyField(Question)
-	category = models.CharField(max_length = 20)
-
 class MultipleChoiceOption(models.Model):
 	choice1 = models.CharField(max_length = 40)
 	choice2 = models.CharField(max_length = 40)
@@ -38,6 +26,18 @@ class MultipleChoiceOption(models.Model):
 class RangeOption(models.Model):
 	lower_bound = models.IntegerField(default = 0) 
 	upper_bound = models.IntegerField(default = 100)
+
+class Question(models.Model):
+	asker = models.ForeignKey(User)
+	text = models.CharField(max_length = 200)
+	question_type = models.CharField(max_length = 20)
+	date = models.DateTimeField(auto_now=True)
+	multiple_choice_options = models.ForeignKey(MultipleChoiceOption, null=True, blank=True, default=None)
+	range_options = models.ForeignKey(RangeOption, null=True, blank=True, default=None)
+
+class Category(models.Model):
+	question = models.ManyToManyField(Question)
+	category = models.CharField(max_length = 20)
 
 class Answer(models.Model):
 	user = models.ForeignKey(User)
