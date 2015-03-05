16d234e7911fb5260bca9a1a557aeb4d0a47b5f4
JoelTham <joel.tham@live.com>
2015-03-05 15:55:14 -0500

    Removed annotate clause tests

--- wildfire/serializers.py
+++ wildfire/serializers.py
@@ -146,7 +146,7 @@ class QuestionSerializer(serializers.ModelSerializer):
 class StatsSerializer(serializers.BaseSerializer):
 	def to_representation(self, obj):
 		answers = Answer.objects.filter(question=obj.pk)
-		regionStats = answers.values('<group by field>').annotate(num_answers=Count(user__region))
+		#regionStats = answers.values('<group by field>').annotate(num_answers=Count(user__region))
 		return{
 			'quick':{
 				'option1': answers.filter(answer = 0).count(),
@@ -169,5 +169,5 @@ class StatsSerializer(serializers.BaseSerializer):
 				'option4': answers.filter(answer = 3,user__gender = "F").count(),
 				'option5': answers.filter(answer = 4,user__gender = "F").count()
 			}
-			'region': regionStats
+			#'region': regionStats
 		}
