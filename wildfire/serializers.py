d51f30304e0ac2d854467e608b3929f90a8297ca
JoelTham <joel.tham@live.com>
2015-03-05 17:33:50 -0500

    Testing annotate clause - 3

--- wildfire/serializers.py
+++ wildfire/serializers.py
@@ -146,7 +146,7 @@ class QuestionSerializer(serializers.ModelSerializer):
 class StatsSerializer(serializers.BaseSerializer):
 	def to_representation(self, obj):
 		answers = Answer.objects.filter(question=obj.pk)
-		#regionStats = answers.values('<group by field>').annotate(num_answers=Count(user__region))
+		answers.values('user__region').annotate(region_count=Count(user__region))
 		return{
 			'quick':{
 				'option1': answers.filter(answer = 0).count(),
@@ -168,6 +168,6 @@ class StatsSerializer(serializers.BaseSerializer):
 				'option3': answers.filter(answer = 2,user__gender = "F").count(),
 				'option4': answers.filter(answer = 3,user__gender = "F").count(),
 				'option5': answers.filter(answer = 4,user__gender = "F").count()
-			}
-			#'region': regionStats
+			},
+			'region': region_count
 		}
